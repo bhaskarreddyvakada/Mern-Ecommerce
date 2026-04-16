d39bfd6b619f24e324723b7491a0b0a1f6babea8
Vamsikrishna072005 <puttavamsikrishna07@gmail.com>
2026-04-16 13:58:41 +0530

    first commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,16 @@
-
-
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Register from './components/Register'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
-
+import Register from './components/Register'
+import Login from './components/login'
 export default function App() {
-  
   return (
     <BrowserRouter>
-      <Navbar/>
-        <Routes>
-          <Route path='/' element/>
-          <Route path='/register' element={<Register/>}/>
-        </Routes>
-    
+    <Navbar/>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </BrowserRouter>
   )
 }
