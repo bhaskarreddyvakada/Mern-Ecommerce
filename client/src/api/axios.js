6e79f9b668950e1008375dbf00bdf009b7e5dd69
bhaskarreddyvakada <153351465+bhaskarreddyvakada@users.noreply.github.com>
2026-04-17 16:47:50 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"https://mern-ecommerce-1miz.onrender.com/"
+    baseURL:"https://mern-ecommerce-1miz.onrender.com/api"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
