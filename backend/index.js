1f68532a81537fb46313d19a5681d63469f7cb42
Vamsikrishna072005 <puttavamsikrishna07@gmail.com>
2026-04-17 16:19:43 +0530

    Day 3 cart

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const mongoose= require("mongoose")
 const app= express()
 const cors = require("cors")
 require("dotenv").config()
-const authRoutes=require("./routes/authRoutes")
+const authRoutes=require("./routes/AuthRoutes")
 const productRoutes=require("./routes/productRoutes")
 
 app.use(cors())
